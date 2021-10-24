bf72872998671e0b40df7cc6885af4d5200aa08b
aurpet <petrauskas.aurimas@gmail.com>
2021-10-24 21:49:21 +0300

    all project files

---
+++ src/test/java/com/tax/app/TaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tax.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
